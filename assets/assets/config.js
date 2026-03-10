378f5250879f02bb5ae71d010f5c74a661bfd0be
msf-portal <msf542395@gmail.com>
2026-03-10 21:14:30 +0900

    Add files via upload

---
+++ assets/assets/config.js
@@ -0,0 +1,23 @@
+// assets/config.js
+window.APP_CONFIG = {
+credentials: {
+     ea: {
+       id: (function(){ const a = ["dmlE","aGE1","S0w="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["em1x","TU5y","WXNz","Wm1W"]; return atob(a.join("")); })()
+     },
+     tool: {
+       id: (function(){ const a = ["azVa","OXQz","ZFk="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["ZFph","N0tQ","Z3N0","dkpT"]; return atob(a.join("")); })()
+     },
+     alert: {
+       id: (function(){ const a = ["WHR6","b1kz","OGs="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["TW13","dVNk","UGdq","QjJI"]; return atob(a.join("")); })()
+     },
+   },
+
+  unlocked: {
+    ea: true,
+    tool: true,
+    alert: true
+  }
+};
