71fc5fc4105bf01eeb6a321381111c8f0ed3962f
msf-portal <msf542395@gmail.com>
2026-03-26 23:31:19 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -2,16 +2,16 @@
 window.APP_CONFIG = {
 credentials: {
      ea: {
-       id: (function(){ const a = ["dmlE","aGE1","S0w="]; return atob(a.join("")); })(),
-       pass: (function(){ const a = ["em1x","TU5y","WXNz","Wm1W"]; return atob(a.join("")); })()
+       id: (function(){ const a = ["NGpl","S1lE","SFI="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["WmtR","WkU4","RVFX","QjV5"]; return atob(a.join("")); })()
      },
      tool: {
-       id: (function(){ const a = ["azVa","OXQz","ZFk="]; return atob(a.join("")); })(),
-       pass: (function(){ const a = ["ZFph","N0tQ","Z3N0","dkpT"]; return atob(a.join("")); })()
+       id: (function(){ const a = ["VmRq","WGdt","Tnc="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["VFRa","SHFh","RkdB","R0E5"]; return atob(a.join("")); })()
      },
      alert: {
-       id: (function(){ const a = ["WHR6","b1kz","OGs="]; return atob(a.join("")); })(),
-       pass: (function(){ const a = ["TW13","dVNk","UGdq","QjJI"]; return atob(a.join("")); })()
+       id: (function(){ const a = ["WWFa","M3BT","MjQ="]; return atob(a.join("")); })(),
+       pass: (function(){ const a = ["OENa","RXB2","TUFZ","dTZI"]; return atob(a.join("")); })()
      },
    },
 
